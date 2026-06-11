608b7f95389fc6713743a7896314ff87015d987f
Sarojtheanalyst <sarojgiri853@gmail.com>
2026-06-11 12:41:24 +0545

    add some dummy_codes

--- demo1.py
+++ demo1.py
@@ -4,3 +4,7 @@ num2=int(input(" enter the second number "))
 
 total=num1+num2
 print(" the total is ",total)
+
+num3=int(input(" enter the third number "))
+total=total+num3
+print(" the total is ",total)
